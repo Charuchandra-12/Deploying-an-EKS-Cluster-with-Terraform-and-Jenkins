2887f8e7d5b1051cd5119fdba32b3461fe41aaf5
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2024-01-07 21:49:24 +0530

    jenkins script updated

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -6,12 +6,12 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
-# Add required dependencies for the jenkins package
-sudo yum amazon-linux-extras install java-openjdk11 -y
+sudo yum install fontconfig java-17-openjdk -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+sudo systemctl status jenkins
 
 # install git
 sudo yum install git -y
