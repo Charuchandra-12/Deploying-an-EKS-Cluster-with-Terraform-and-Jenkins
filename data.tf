0aa636076d0626ba331a3d0bad8c4e6e04e51f0f
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2024-01-08 11:53:19 +0530

    code updated for ec2 image and jenkins script

--- data.tf
+++ data.tf
@@ -4,7 +4,7 @@ data "aws_ami" "custom_ami" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20231207"]
+    values = ["amzn2-ami-kernel-*-hvm-*-x86_64-gp2"]
   }
 
   filter {
