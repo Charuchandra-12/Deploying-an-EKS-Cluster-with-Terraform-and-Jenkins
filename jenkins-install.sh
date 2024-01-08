184871e1a6fcc5dfcfab937336b20f74e69b82e2
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2024-01-08 13:26:09 +0530

    script updated

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,44 +1,17 @@
 #!/bin/bash
 
-# Update the system, Install Java
-sudo apt-get update -y && sudo apt install openjdk-11-jdk -y
-# Add Jenkins Repository
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
 # Install Jenkins
-sudo apt update -y && sudo apt install jenkins -y && sudo systemctl status jenkins 
-# If not active or running then run:- sudo systemctl enable --now jenkins
-
+sudo snap install jenkins --classic
 
 # Install terraform
-sudo apt-get update -y && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update -y
-sudo apt-get install terraform -y
-
+sudo snap install terraform --classic 
 
 # Install kubectl
-# apt-transport-https may be a dummy package; if so, you can skip that package
-sudo apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install -y kubectl
+sudo snap install kubectl --classic
 
 # Configuring Firewall
+# sudo ufw allow http
 sudo ufw allow 8080
 sudo ufw allow ssh
-# sudo ufw allow http
 sudo ufw enable
+
