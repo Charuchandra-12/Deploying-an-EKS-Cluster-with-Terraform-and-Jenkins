36eb29a7d1108ef1d3e10a734b39d6684e212ada
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2024-01-09 12:05:04 +0530

    script updated

--- Jenkins_Terraform/jenkins-install.sh
+++ Jenkins_Terraform/jenkins-install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-sudo apt update
 # Install Jenkins
-sudo snap install jenkins --classic
+# sudo snap install jenkins --classic
+sudo apt update sudo apt install openjdk-11-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y && sudo apt install jenkins -y && sudo systemctl status jenkins 
 
 # Install terraform
 sudo snap install terraform --classic 
 
 # Install kubectl
-sudo snap install kubectl --classic
+# sudo snap install kubectl --classic
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 
 # Configuring Firewall
 # sudo ufw allow http
