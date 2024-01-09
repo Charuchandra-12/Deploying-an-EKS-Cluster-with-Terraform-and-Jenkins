3648d6849fff82f75dde8256c848617df69ad7b8
Chinmay <92244743+Charuchandra-12@users.noreply.github.com>
2024-01-09 12:15:20 +0530

    script updated

--- Jenkins_Terraform/jenkins-install.sh
+++ Jenkins_Terraform/jenkins-install.sh
@@ -2,13 +2,13 @@
 
 # Install Jenkins
 # sudo snap install jenkins --classic
-sudo apt update sudo apt install openjdk-11-jdk -y
+sudo apt update && sudo apt install openjdk-11-jdk -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update -y && sudo apt install jenkins -y && sudo systemctl status jenkins 
+sudo apt update && sudo apt install jenkins -y && sudo systemctl status jenkins 
 
 # Install terraform
 sudo snap install terraform --classic 
